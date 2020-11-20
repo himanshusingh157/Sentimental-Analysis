a19a34762d39134451d669da7910369a7f89c78a
Himanshu Singh <68109107+himanshusingh157@users.noreply.github.com>
2020-11-20 16:08:24 +0530

    Update Glove_RNN.py

--- Glove_RNN.py
+++ Glove_RNN.py
@@ -4,6 +4,9 @@ import torch.optim as optim
 import torch.nn as nn
 import random
 
+#Using Glove embedding
+#loading datasets
+#unknown token initialised to random vector from normal distribution rather than all zeros
 TEXT=data.Field(tokenize='spacy', include_lengths = True)
 LABEL=data.LabelField(dtype=torch.float)
 
@@ -14,14 +17,11 @@ TEXT.build_vocab(train_data,vectors="glove.6B.100d",unk_init=torch.Tensor.normal
 LABEL.build_vocab(train_data)
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-
 train_iter, valid_iter, test_iter=data.BucketIterator.splits((train_data, valid_data, test_data),batch_size=64,sort_within_batch=True,device=device)
 
 #embeddig dimension=100
 #hidden(h_t) dimension=256
-#number of layers=2
-#birectional=True
-class LSTM(nn.Module):
+class RNN(nn.Module):
     def __init__(self, vocab_size, pad_idx):
         super().__init__()
         self.embedding=nn.Embedding(vocab_size, 100, padding_idx=pad_idx)
@@ -35,16 +35,17 @@ class LSTM(nn.Module):
         output, output_lengths=nn.utils.rnn.pad_packed_sequence(output) #unpacking
         return self.fc(hidden.squeeze(0))
 
-model=LSTM(len(TEXT.vocab),TEXT.vocab.stoi[TEXT.pad_token])
+model=RNN(len(TEXT.vocab),TEXT.vocab.stoi[TEXT.pad_token])
 
+#Copying GloVe embedding into our model embedding
 model.embedding.weight.data.copy_(TEXT.vocab.vectors)
 
 model.embedding.weight.data[TEXT.vocab.stoi[TEXT.unk_token]]=torch.zeros(100)
 model.embedding.weight.data[TEXT.vocab.stoi[TEXT.pad_token]]=torch.zeros(100)
 model=model.to(device)
 
+#Setting optimizer and loss function
 optimizer=optim.Adam(model.parameters())
-
 criterion=nn.BCEWithLogitsLoss()
 criterion=criterion.to(device)
 
@@ -84,6 +85,7 @@ def evaluate(model, iter, criterion):
             total_acc+=acc.item()
     return total_loss / len(iter), total_acc / len(iter)
 
+#training model
 epochs=20
 best_loss=float('inf')
 for epoch in range(epochs):
@@ -95,8 +97,7 @@ for epoch in range(epochs):
     
     print(f'Epoch: {epoch+1}   Train Loss: {train_loss:.4f}   Train Accuracy: {train_acc*100:.2f}%   Valiation Loss: {valid_loss:.4f}   Validtion Accuracy: {valid_acc*100:.2f}%')
 
-
+#testing the model
 model.load_state_dict(torch.load('Glove_RNN.pt'))
 test_loss, test_acc = evaluate(model, test_iter, criterion)
 print(f'Test Loss: {test_loss:.4f}   Test Acc: {test_acc*100:.2f}%')
-
